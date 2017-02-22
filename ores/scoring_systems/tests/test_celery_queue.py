86132548f34c7f8dd9d3ae8fbbed267e0c800f17
Adam Roses Wight <awight@wikimedia.org>
2017-02-22 14:38:09 -0800

    Unit test for cached score

--- ores/scoring_systems/tests/test_celery_queue.py
+++ ores/scoring_systems/tests/test_celery_queue.py
@@ -1,5 +1,7 @@
 import celery
 import celerytest
+import mock
+
 from ..celery_queue import CeleryQueue
 from .util import fakewiki, wait_time, test_scoring_system
 
@@ -24,3 +26,24 @@ def test_timeout():
     assert 'error' in score_doc['scores'][1], str(score_doc['scores'])
     assert 'Timed out after' in score_doc['scores'][1]['error']['message'], \
            score_doc['scores'][1]['error']['message']
+
+
+def test_lookup_cached():
+    application = celery.Celery(__name__)
+
+    # Set cached task result.
+    stored_result = 123
+    cache = mock.MagicMock()
+    cache.lookup.return_value = stored_result
+
+    scoring_system = CeleryQueue(
+        {'fakewiki': fakewiki}, application=application, timeout=0.10, score_cache=cache)
+    celerytest.start_celery_worker(application, concurrency=1)
+
+    score_doc = scoring_system.score(
+        "fakewiki", ["fake"], [1], injection_caches={1: {wait_time: 0.10}})
+    expected_result = {
+        'scores': {1: {'fake': {'score': stored_result}}},
+        'models': {'fake': {'version': 'fake version'}},
+    }
+    assert expected_result == score_doc
